a048cefc50086b9273b3548fcfe8a684d34d8969
KobayashiYoh <y.kobayashi@4nonome.com>
2022-12-30 19:39:36 +0900

    Riverpodで状態管理するプロパティを定義したOmikujiStateクラスを作成

---
+++ lib/omikuji_state.dart
@@ -0,0 +1,22 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'omikuji_state.freezed.dart';
+
+@freezed
+class OmikujiState with _$OmikujiState {
+  const factory OmikujiState({
+    required bool isLoading,
+    required bool hasError,
+    required String fortune,
+    required String message,
+    required double opacityLevel,
+  }) = _OmikujiState;
+}
+
+const OmikujiState kInitialOmikujiState = OmikujiState(
+  isLoading: false,
+  hasError: false,
+  fortune: '',
+  message: '',
+  opacityLevel: 0.0,
+);
