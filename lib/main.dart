c7f7c4493a8ec3c30499193385940444f3b3da43
KobayashiYoh <y.kobayashi@4nonome.com>
2022-12-30 19:38:18 +0900

    Riverpod導入に必要なpackageのインストール

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:omikuji2021/main_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
