a825e4b58b295f850e34d7b5ffcb71b721a7c2e0
KobayashiYoh <y.kobayashi@4nonome.com>
2022-12-31 17:12:01 +0900

    音源のクレジットを追記

--- lib/constants/sound_path.dart
+++ lib/constants/sound_path.dart
@@ -4,4 +4,8 @@ class SoundPath {
   static const String bgm = '${_directoryPath}bgm.mp3';
   static const String tap = '${_directoryPath}tap.mp3';
   static const String shamisen = '${_directoryPath}shamisen.mp3';
+
+  // 使用した音源
+  // https://otologic.jp/free/se/instruments-japanese01.html
+  // https://on-jin.com/sound/index.php?kensaku=%E5%92%8C%E9%A2%A8
 }
