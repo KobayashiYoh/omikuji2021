ae28aafbbc79cb6968345ae5425affd1efe5c76a
KobayashiYoh <y.kobayashi@4nonome.com>
2023-01-02 11:42:52 +0900

    アラートのボタンをタップでミュートをオフにできるように修正

--- lib/views/omikuji_page.dart
+++ lib/views/omikuji_page.dart
@@ -1,16 +1,49 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:omikuji_app/providers/audio_notifier.dart';
 import 'package:omikuji_app/providers/omikuji_notifier.dart';
+import 'package:omikuji_app/ui_components/indigo_elevated_button.dart';
+import 'package:omikuji_app/ui_components/web_initial_alert_dialog.dart';
 import 'package:omikuji_app/views/loading_view.dart';
 import 'package:omikuji_app/views/network_error_view.dart';
 import 'package:omikuji_app/views/result_view.dart';
 
-class OmikujiPage extends ConsumerWidget {
+class OmikujiPage extends ConsumerStatefulWidget {
   const OmikujiPage({Key? key}) : super(key: key);
+  @override
+  OmikujiPageState createState() => OmikujiPageState();
+}
+
+class OmikujiPageState extends ConsumerState<OmikujiPage> {
+  void _onPressedStartButton() {
+    final notifier = ref.read(audioProvider.notifier);
+    notifier.onPressedMuteButton();
+    Navigator.pop(context);
+  }
+
+  void _showWebInitialAlertDialog() {
+    Future(() {
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => WebInitialAlertDialog(
+          onPressed: _onPressedStartButton,
+        ),
+      );
+    });
+  }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  void initState() {
+    super.initState();
+    if (kIsWeb) {
+      _showWebInitialAlertDialog();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final omikujiState = ref.watch(omikujiProvider);
     final audioState = ref.watch(audioProvider);
     final omikujiNotifier = ref.watch(omikujiProvider.notifier);
@@ -46,18 +79,9 @@ class OmikujiPage extends ConsumerWidget {
                 const Spacer(),
                 SizedBox(
                   height: 48.0,
-                  child: ElevatedButton(
+                  child: IndigoElevatedButton(
                     onPressed: omikujiNotifier.drawOmikuji,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.indigo[900],
-                    ),
-                    child: const Text(
-                      'おみくじを引く',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.white,
-                      ),
-                    ),
+                    buttonText: 'おみくじを引く',
                   ),
                 ),
                 const SizedBox(height: 32.0),
